efaa5013020ff0bdc1843c110b79116db95dde27
Scott <scott.adams@raspberrypi.org>
2024-11-21 13:31:36 +0000

    Regular automated backup

--- vscode_backup.sh
+++ vscode_backup.sh
@@ -7,9 +7,12 @@ cd $HOME/Documents/repos/Personal/vscode
 
 # run the backup
 /usr/local/bin/code --list-extensions > VSfile
-# remove and make a fresh copy of .vscode
+# remove and make a fresh copy of .vscode & User
 rm -r .vscode
 cp -R $HOME/Documents/repos/.vscode .
+mkdir -p User
+rm -r User/*
+cp -R ~/Library/Application\ Support/Code/User/settings.json User/
 
 # add to git repo
 git add .
