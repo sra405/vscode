0edcec50ed1ac0080f0268a30cb69345e2a82be8
Scott Adams <scott.adams@neuralfoundry.co.uk>
2025-06-25 16:20:51 +0100

    Regular automated backup

--- vscode_restore.sh
+++ vscode_restore.sh
@@ -5,16 +5,8 @@ echo "VSCode Restorer-erer"
 # change to my backup location
 cd $HOME/Documents/repos/Personal/vscode
 
-# run the backup
-# /usr/local/bin/code --list-extensions > VSfile
-# remove and make a fresh copy of .vscode
-#mkdir -p $HOME/Documents/repos/.vscode
+# Restore the config
 cp -R .vscode $HOME/Documents/repos
 
-# add to git repo
-#git add .
-#git commit -m "Regular automated backup"
-#git push
-
 # Restore extensions
-cat VSfile | while read line; do /usr/local/bin/code --install-extension "$line"; done
+cat VSfile | while read line; do /opt/homebrew/bin/code --install-extension "$line"; done
