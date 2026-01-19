7791145d8402e85f9ba461df684b80bccbea5b62
Scott Adams <scott.adams@neuralfoundry.co.uk>
2026-01-19 11:27:42 +0000

    Regular automated backup

--- vscode_backup.sh
+++ vscode_backup.sh
@@ -6,7 +6,7 @@ echo "VSCode Back-Uperer"
 cd $HOME/Documents/repos/Personal/vscode
 
 # run the backup
-/usr/local/bin/code --list-extensions > VSfile
+/opt/homebrew/bin/code --list-extensions > VSfile
 # remove and make a fresh copy of .vscode & User
 rm -r .vscode
 cp -R $HOME/Documents/repos/.vscode .
